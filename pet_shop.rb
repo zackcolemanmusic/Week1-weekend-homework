226668fa98da1a8ca3c65dcc6894c0c1d828556e
Zack <Zackcolemanmusic@gmail.com>
2017-10-02 08:35:07 +0100

    Test 12 Passed

--- pet_shop.rb
+++ pet_shop.rb
@@ -39,3 +39,11 @@ def find_pet_by_name(pet_shop,name)
   end
     return nil
 end
+
+def remove_pet_by_name(pet_shop, name)
+  for pet in pet_shop[:pets]
+    if pet[:name] == name
+      pet_shop[:pets].delete(pet)
+    end
+  end
+end
